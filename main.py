d833c28fb5764cc4c042217d5f09f2df6b706bfc
sohaihub <soha.r.ihub@snsgroups.com>
2025-04-02 10:43:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ try:
         "type": st.secrets["type"],
         "project_id": st.secrets["project_id"],
         "private_key_id": st.secrets["private_key_id"],
-        "private_key": st.secrets["private_key"].replace("\\n", "\n"),
+        "private_key": st.secrets["private_key"].replace("-----BEGIN PRIVATE KEY-----\\n", "-----BEGIN PRIVATE KEY-----\n").replace("\\n", "\n"),
         "client_email": st.secrets["client_email"],
         "client_id": st.secrets["client_id"],
         "auth_uri": st.secrets["auth_uri"],
         "token_uri": st.secrets["token_uri"],
         "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
-        "client_x509_cert_url": st.secrets["client_x509_cert_url"]
+        "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+        "universe_domain": st.secrets.get("universe_domain", "")
     }
 
     client = gspread.service_account_from_dict(creds)
